17427868b6650bc17fa88e5e24b6b2e95ad50a7d
Linghui Zeng <lh.zeng@hotmail.com>
2020-08-24 00:40:34 +0800

    my first code commit

---
+++ conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HELPER_CONTACT_NAME: 'your_helper_contact_name',
+  // puppet_padplus Token
+  token: 'your_puppet_padplus_token',
+  // 机器人名字
+  name: 'your_bot_name',
+  // 群名
+  topic: '营销小助手', 
+  // 好友验证自动通过关键字
+  addFriendKeywords: ['求助', '求入群'],
+  // 百度语音转文本 API 调用 Token
+  access_token: "your_baidu_token",
+}
